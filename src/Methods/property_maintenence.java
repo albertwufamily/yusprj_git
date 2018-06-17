599850e1db9cae6c10ea193fce11593ad464b330
albert <lili@lili-PC.fios-router.home>
2018-06-17 11:20:36 -0400

    learned in 4/22/2018 class

---
+++ src/Methods/property_maintenence.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Methods;
+
+/**
+ *
+ * @author lili
+ */
+public class property_maintenence {
+    public static void print_properties (int mowed, int fertilized)
+        {
+        System.out.println(mowed + " properties were mowed");
+        System.out.println(fertilized + " properties were fertilized");
+        }
+    
+    public static void mower_income(double income)
+        {
+        double income_tax = income*1/4;
+        double net_income = income-income_tax;
+        System.out.println(income);
+        System.out.println(income_tax);
+        System.out.println(net_income);
+        }
+    public static void mower_report (double income, int fertilized, int mowed)
+        {
+        System.out.println("lawn Mower Monthly Report");
+        System.out.println();
+        print_properties(10, 12);
+        mower_income(20);
+        }
+    public static void print_removal(int driveways, int sidewalks, boolean blower)
+        {
+        System.out.println(driveways);
+        System.out.println(sidewalks);
+        System.out.println("I used a snowblower" + blower);
+        }
+    public static void print_snow_income (double income)
+        {
+        System.out.println(income);
+        }
+    public static void print_snow_report ()
+}
---
+++ src/classes/person.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+/**
+ *
+ * @author lili
+ */
+public class person 
+{
+    String name;
+    int ssn;
+    public static void main (String [] args)
+    {
+    person p1=new person ();
+    person p2=new person ();
+    person p3=new person ();
+    p1.name="Cyndy";
+    p1.ssn=123;
+    p2.name="Aaron";
+    p2.ssn=456;
+    p3.name="Albert";
+    p3.ssn=789;//
+    }
+}
